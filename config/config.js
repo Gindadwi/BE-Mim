2826eb52d61502e4a5f0ff80c6e734be1abddbe4
Gindadwi <gindadwi144@gmail.com>
2025-03-04 04:19:07 -0800

    config

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOSTNAME } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: DB_HOSTNAME,
+    host: DB_HOST,
     dialect: "mysql",
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: DB_HOSTNAME,
+    host: DB_HOST,
     dialect: "mysql",
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: DB_HOSTNAME,
+    host: DB_HOST,
     dialect: "mysql",
   },
 };
